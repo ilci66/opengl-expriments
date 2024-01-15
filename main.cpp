a753d6825dab5bf1caf0c84a1fa6498ee62005f1
Ilker Akbiyik <akbiyik.ilker7@gmail.com>
2024-01-15 22:53:15 +0100

    Add square using indices

--- main.cpp
+++ main.cpp
@@ -311,22 +311,32 @@ int main()
   glDeleteShader(fragmentShader);
 
   float vertices[] = {
-      -0.9f, -0.9f, 0.0f,
-      -0.9f, -0.1f, 0.0f,
-      -0.5f, -0.5f, 0.0f,
-      // The second triangle in the second area
-      0.9f, 0.9f, 0.0f,
-      0.9f, 0.1f, 0.0f,
-      0.5f, 0.5f, 0.0f};
+      0.5f, 0.5f, 0.0f,   // top right
+      0.5f, -0.5f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f, // bottom left
+      -0.5f, 0.5f, 0.0f   // top left
+  };
 
-  unsigned int VBO, VAO;
+  unsigned int indices[] = {
+      // note that we start from 0!
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
+  };
+
+  // EBO is Element Buffer Object
+  unsigned int VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   // Bind VAO first, then bind & set vertex buffers, then configure vertex
   glBindVertexArray(VAO);
+
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
@@ -345,7 +355,9 @@ int main()
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
     // glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawArrays(GL_TRIANGLES, 0, 6); // for two triangles
+    // glDrawArrays(GL_TRIANGLES, 0, 6); // for two triangles
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     // glBindVertexArray(0); // no need to unbind it every time
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
