be4353f6a5afea2e3dc630ad157ab957305e8634
Ilker Akbiyik <akbiyik.ilker7@gmail.com>
2024-01-23 23:42:08 +0100

    Add multiple textures for the same rectangle

--- main.cpp
+++ main.cpp
@@ -96,17 +96,21 @@ int main()
 
   // load and create a texture
   // -------------------------
-  unsigned int texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+  unsigned int texture1, texture2;
+  // texture 1
+  // ---------
+  glGenTextures(0, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
   // set the texture wrapping parameters
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   // set texture filtering parameters
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   // load image, create texture and generate mipmaps
   int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(true);
+
   unsigned char *data = stbi_load((parentPath / "container.jpg").c_str(), &width, &height, &nrChannels, 0);
   if (data)
   {
@@ -117,8 +121,37 @@ int main()
   {
     std::cout << "Failed to load texture" << std::endl;
   }
-  stbi_image_free(data); // free the memory after creating corresponding mipmaps
+  stbi_image_free(data);
+  // texture 2
+  // ---------
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+  // set the texture wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load image, create texture and generate mipmaps
 
+  data = stbi_load((parentPath / "awesomeface.png").c_str(), &width, &height, &nrChannels, 0);
+  if (data)
+  {
+    // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+  // either set it manually like so:
+  glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+  // or set it via the texture class
+  ourShader.setInt("texture2", 1);
   // render loop
   // -----------
   while (!glfwWindowShouldClose(window))
@@ -133,7 +166,10 @@ int main()
     glClear(GL_COLOR_BUFFER_BIT);
 
     // bind Texture
-    glBindTexture(GL_TEXTURE_2D, texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture2);
 
     // render container
     ourShader.use();
