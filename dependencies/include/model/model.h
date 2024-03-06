12ebe895da3ea17c7d0786fb8c1eac1cd4b390a7
Ilker Akbiyik <akbiyik.ilker7@gmail.com>
2024-03-07 00:03:05 +0100

    fix(model) remove aiProcess_FlipUVs option from importer.readfile

--- dependencies/include/model/model.h
+++ dependencies/include/model/model.h
@@ -53,6 +53,9 @@ private:
     // read file via ASSIMP
     Assimp::Importer importer;
     const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+    // Removing aiProcess_FlipUVs rendered better looking textures
+    // aiProcess_FlipUVs is to be added when using Direct3D I guess (windows)
+    // const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals  | aiProcess_CalcTangentSpace);
     // check for errors
     if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
     {
--- main.cpp
+++ main.cpp
@@ -89,8 +89,8 @@ int main()
 
   Shader ourShader(vertFilePathStr, fragFilePathStr);
 
-  // Model ourModel(parentPath / "backpack/backpack.obj");
-  Model ourModel(parentPath / "pony-cartoon/exported/Pony_cartoon.obj");
+  Model ourModel(parentPath / "backpack/backpack.obj");
+  // Model ourModel(parentPath / "pony-cartoon/exported/Pony_cartoon.obj");
 
   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // wireframe
 
