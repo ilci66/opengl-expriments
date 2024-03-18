7b71900d987cdb105dc50a6e87630f732b2cf372
Ilker Akbiyik <akbiyik.ilker7@gmail.com>
2024-03-18 22:04:36 +0100

    add dependecies and libraries copy from basics

---
+++ advanced-opengl/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ilkerakbiyik/Desktop/dev-stuff/opengl-experiments/advanced-opengl/glad.c" "CMakeFiles/app.dir/glad.c.o" "gcc" "CMakeFiles/app.dir/glad.c.o.d"
+  "/Users/ilkerakbiyik/Desktop/dev-stuff/opengl-experiments/advanced-opengl/camera.cpp" "CMakeFiles/app.dir/camera.cpp.o" "gcc" "CMakeFiles/app.dir/camera.cpp.o.d"
+  "/Users/ilkerakbiyik/Desktop/dev-stuff/opengl-experiments/advanced-opengl/main.cpp" "CMakeFiles/app.dir/main.cpp.o" "gcc" "CMakeFiles/app.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
